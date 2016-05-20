39b0ac76fca125569ce8757d1f9a30157529c0b8
Stanislaw Bogatkin <sbogatkin@mirantis.com>
2016-05-20 15:36:53 +0300

    Implement main part of functionality

--- fuel-yaql/fuyaql.py
+++ fuel-yaql/fuyaql.py
@@ -13,6 +13,7 @@ Arguments:
     CLUSTER_ID      Cluster ID for which YAQL data will be gathered
 """
 
+import json
 import logging
 from docopt import docopt
 from nailgun import consts
@@ -24,6 +25,15 @@ from nailgun.db.sqlalchemy.models import Cluster
 from nailgun.db.sqlalchemy.models import Task
 from nailgun.orchestrator import deployment_serializers
 from nailgun.task.task import ClusterTransaction
+
+reserved_commands = {
+    ':show cluster': 'show_cluster',
+    ':show nodes': 'show_nodes',
+    ':show node': 'show_node',
+    ':use cluster': 'use_cluster',
+    ':use node': 'use_node',
+    ':load lastrun': 'update_old_context',
+}
 
 
 class Options:
@@ -56,8 +66,19 @@ class Fyaql:
         self.options = options.options
         self.logger = options.logger
         self.cluster_id = self.options['CLUSTER_ID']
+        self.node_id = 'master'
         self.cluster = self.get_cluster()
+        self.logger.debug('Cluster instance is: %s', self.cluster)
         self.nodes_to_deploy = self.get_nodes_to_deploy()
+        self.logger.debug('Nodes to deploy are: %s', self.nodes_to_deploy)
+        self.supertask = None
+        self.deployment = None
+        self.deployment_info = None
+        self.expected_state = None
+        self.deployment_tasks = None
+        self.current_state = None
+        self.context = None
+        self.yaql_engine = None
 
     def get_cluster(self):
         return db().query(Cluster).get(self.cluster_id)
@@ -67,78 +88,152 @@ class Fyaql:
             objects.Cluster.get_nodes_not_for_deletion(self.cluster).all()
         )
 
-    def run(self):
-        self.logger.debug('Cluster instance is: %s', self.cluster)
-        if not self.cluster:
-            return
-        supertask = Task(name=consts.TASK_NAMES.deploy, cluster=self.cluster,
-                         status=consts.TASK_STATUSES.pending)
-        self.logger.debug('Supertask instance is: %s', supertask)
-        task_deployment = supertask.create_subtask(
+    def get_supertask(self):
+        self.supertask = Task(
+            name=consts.TASK_NAMES.deploy, cluster=self.cluster,
+            status=consts.TASK_STATUSES.pending)
+        self.logger.debug('Supertask instance is: %s', self.supertask)
+
+    def get_deployment_cycle(self):
+        self.deployment = self.supertask.create_subtask(
                 name=consts.TASK_NAMES.deployment,
                 status=consts.TASK_STATUSES.pending
             )
-        self.logger.debug('Deployment task instance is: %s', task_deployment)
-        self.logger.debug('Nodes to deploy are: %s', self.nodes_to_deploy)
-        deployment_info = deployment_serializers.serialize_for_lcm(
-            task_deployment.cluster,
+        self.logger.debug('Deployment task instance is: %s', self.deployment)
+
+    def get_deployment_info(self):
+        self.deployment_info = deployment_serializers.serialize_for_lcm(
+            self.deployment.cluster,
             self.nodes_to_deploy
         )
-        self.logger.debug('Deployment info is: %s', deployment_info)
-        expected_state = ClusterTransaction._save_deployment_info(
-            task_deployment,
-            deployment_info
+        self.logger.debug('Deployment info is: %s', self.deployment_info)
+
+    def get_expected_state(self):
+        self.expected_state = ClusterTransaction._save_deployment_info(
+            self.deployment,
+            self.deployment_info
         )
-        self.logger.debug('Expected state is %s', expected_state)
-        deployment_tasks = objects.Cluster.get_deployment_tasks(
-            task_deployment.cluster,
+        self.logger.debug('Expected state is %s', self.expected_state)
+
+    def get_tasks(self):
+        self.deployment_tasks = objects.Cluster.get_deployment_tasks(
+            self.deployment.cluster,
             None
         )
-        self.logger.debug('Deployment tasks are %s', deployment_tasks)
+        self.logger.debug('Deployment tasks are %s', self.deployment_tasks)
 
+    def get_current_state(self):
+        last_run = objects.TransactionCollection.get_last_succeed_run(
+            self.cluster)
+        self.current_state = last_run.deployment_info
+        self.logger.debug('Current state is: %s', self.current_state)
 
-        ignored_types = {
-            consts.ORCHESTRATOR_TASK_TYPES.skipped,
-            consts.ORCHESTRATOR_TASK_TYPES.group,
-            consts.ORCHESTRATOR_TASK_TYPES.stage,
-        }
-
-        tasks_names = [t['id'] for t in deployment_tasks
-                       if t['type'] not in ignored_types]
-        transaction_collection = objects.TransactionCollection
-        transactions = (
-            transaction_collection.get_successful_transactions_per_task(
-                task_deployment.cluster.id, tasks_names)
-        )
-        current_state = {
-            task_id: objects.Transaction.get_deployment_info(tr)
-            for tr, task_id in transactions
-        }
-        self.logger.debug('Current state is: %s', current_state)
-
-        _transaction_context = lcm.TransactionContext(expected_state, current_state)
-
-        self._yaql_context = yaql_ext.create_context(
+    def get_contexts(self):
+        main_yaql_context = yaql_ext.create_context(
             add_serializers=True, add_datadiff=True
         )
-        self._yaql_engine = yaql_ext.create_engine()
-        child_context = self._yaql_context.create_child_context()
-        try:
-            child_context['$%new'] = _transaction_context.get_new_data('1')
-        except KeyError:
-            child_context['$%new'] = _transaction_context.get_new_data('master')
-        try:
-            child_context['$%old'] = _transaction_context.get_old_data('1', '1')
-        except KeyError:
-            child_context['$%old'] = {}
+        self.context = main_yaql_context.create_child_context()
+        self.update_contexts()
 
-        parsed_exp = self._yaql_engine('changed($)')
-        self.logger.debug('parsed exp is: %s', parsed_exp)
-        res = parsed_exp.evaluate(data=child_context['$%new'], context=child_context)
-        self.logger.debug('Evaluation result is: %s', res)
+    def update_contexts(self):
+        try:
+            self.context['$%new'] = self.expected_state[self.node_id]
+        except KeyError:
+            self.context['$%new'] = self.expected_state['master']
+        try:
+            self.context['$%old'] = self.current_state[self.node_id]
+        except KeyError:
+            self.context['$%old'] = {}
+
+    def create_evaluator(self):
+        self.yaql_engine = yaql_ext.create_engine()
+
+    def evaluate(self, yaql_expression):
+        try:
+            parsed_exp = self.yaql_engine(yaql_expression)
+            self.logger.debug('parsed exp is: %s', parsed_exp)
+            res = parsed_exp.evaluate(data=self.context['$%new'],
+                                      context=self.context)
+            self.logger.debug('Evaluation result is: %s', res)
+        except:
+            self.logger.debug('Exception caught')
+            res = '<Cannot evaluate this, exception caught>'
+        return res
+
+    def create_structure(self):
+        if not self.cluster:
+            return
+        self.get_supertask()
+        self.get_deployment_cycle()
+        self.get_deployment_info()
+        self.get_expected_state()
+        self.get_tasks()
+        self.get_current_state()
+        self.get_contexts()
+        self.create_evaluator()
+
+    def parse_command(self, command):
+        if command.startswith(':show'):
+            return command, None
+        data = command.split(' ')
+        value = data.pop()
+        command = ' '.join(data)
+        return command, value
+
+    def show_cluster(self):
+        print('Cluster id is: %s, name is: %s' %
+              (self.cluster.id, self.cluster.name))
+
+    def show_nodes(self):
+        nodes_ids = {}
+        for node in self.cluster.nodes:
+            nodes_ids[node.id] = str(', '.join(node.all_roles))
+        print('Cluster has nodes with ids: %s' % nodes_ids)
+
+    def show_node(self):
+        print('Currently used node id is: %s' % self.node_id)
+
+    def use_cluster(self, cluster_id):
+        pass
+
+    def use_node(self, node_id):
+        self.node_id = node_id
+        self.update_contexts()
+
+    def load_lastrun(self):
+        pass
+
+    def run_internal_command(self, command, value):
+        if command not in reserved_commands:
+            print('Unknown internal command')
+            return
+        if value:
+            getattr(self, reserved_commands[command])(value)
+        else:
+            getattr(self, reserved_commands[command])()
+
+    def get_console(self):
+        command = True
+
+        while command != 'exit':
+            try:
+                command = raw_input('fuel-yaql> ')
+            except EOFError:
+                return
+            if not command:
+                continue
+
+            if command.startswith(':'):
+                command, value = self.parse_command(command)
+                self.run_internal_command(command, value)
+            else:
+                result = self.evaluate(command)
+                print json.dumps(result, indent=4)
 
 
 if __name__ == '__main__':
     opts = Options()
     interpret = Fyaql(opts)
-    interpret.run()
+    interpret.create_structure()
+    interpret.get_console()
+
